4dc38854eed389f6f79333b46599b2e4eb47d092
Messi123L <housnianas77@gmail.com>
2024-04-27 13:50:37 +0200

    first commit

---
+++ backend/data.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcryptjs';
+
+const data = {
+  users: [
+    {
+      name: 'elmachhoune',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'mohamed',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
+  products: [
+    {
+      // _id: '1',
+      name: 'Nike Slim shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+  ],
+};
+export default data;
---
+++ frontend/src/data.js
@@ -0,0 +1,17 @@
+const data = {
+  products: [
+    {
+      name: 'Airpods i12 TWS',
+      slug: 'i12 TWS',
+      category: 'Shirts',
+      image: '/images/p1.jpg', // 679px × 829px
+      price: 120,
+      countInStock: 10,
+      brand: 'Apple',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality headphones',
+    },
+  ],
+};
+export default data;
